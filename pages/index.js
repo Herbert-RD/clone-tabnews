183f78d188e74a0f5c65f063404521428aec8640
Herbert Richard <hebert.richard0800@gmail.com>
2024-03-19 09:56:44 -0300

    Adiciona sub texto Binho Beriba

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 function Home(){
   return (
-    <h1>Vitoria, de todas as irmãs que eu tenho você é a melhor 😻🐱👯</h1>
-  )
+    <div>
+      <h1>Vitoria, de todas as irmãs que eu tenho você é a melhor 😻🐱👯</h1>
+      <h2>Binho Beriba</h2>
+    </div>
+
+    )
 }
 
 

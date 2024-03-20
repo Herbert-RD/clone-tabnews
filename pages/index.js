c68f2f119201caafade0b2e8173f1a8fbad062e3
Herbert Richard <hebert.richard0800@gmail.com>
2024-03-20 08:52:41 -0300

    Push study

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ function Home(){
       <h1>Vitoria, de todas as irmãs que eu tenho você é a melhor 😻🐱👯</h1>
       <h2>Binho Beriba</h2>
       <h2>Eu mesmo</h2>
+      <h2>Push Study</h2>
     </div>
 
     )
